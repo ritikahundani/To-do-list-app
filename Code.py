3b48588523e625848e3ccf9a74874489bb9d82a0
ritikahundani <146649852+ritikahundani@users.noreply.github.com>
2024-11-12 09:17:53 +0530

    Code.py
    Running this code opens a new window to access add, delete and mark as completed functions. When you click on add task a new window opens where you enter the task and again click on add task the task will we added to the list. When you select a task and then click on delete or mark as completed, the task will be deleted or marked as completed resp.

--- Code.py
+++ Code.py
@@ -1,50 +1,72 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QListWidget 
+from  tkinter import * 
+import tkinter.messagebox
 
-class ToDoApp (QWidget):
-	def __init__(self):
+#creating the initial window
+window=Tk()
+#giving a title
+window.title("DataFlair Python To-Do List APP")
 
-		super().__init__()
-		self.setWindowTitle("To-Do List App")
-		self.setGeometry (200, 200, 400, 300)
-		self.tasks = []
-		self.layout = QVBoxLayout ()
-		self.input_field = QLineEdit ()
-		self.add_button = QPushButton("Add Task")
-		self.delete_button=QPushButton("Delete Task")
-		self.task_list = QListWidget()
-		self.layout.addWidget(self.input_field)
-		self.layout.addWidget(self.add_button)
-		self.layout.addWidget(self.delete_button)
-		self.layout.addWidget(self.task_list)
-		self.add_button.clicked.connect(self.add_task)
-		self.delete_button.clicked.connect(self.delete_task) 
-		self.setLayout (self.layout)
 
-	def add_task (self):
+#Frame widget to hold the listbox and the scrollbar
+frame_task=Frame(window)
+frame_task.pack()
+def entertask():
+    #A new window to pop up to take input
+    input_text=""
+    def add():
+        input_text=entry_task.get(1.0, "end-1c")
+        if input_text=="":
+            tkinter.messagebox.showwarning(title="Warning!",message="Please Enter some Text")
+        else:
+            listbox_task.insert(END,input_text)
+            #close the root1 window
+            root1.destroy()
+    root1=Tk()
+    root1.title("Add task")
+    entry_task=Text(root1,width=40,height=4)
+    entry_task.pack()
+    button_temp=Button(root1,text="Add task",command=add)
+    button_temp.pack()
+    root1.mainloop()
+    
 
-		task = self.input_field.text()
+#function to facilitate the delete task from the Listbox
+def deletetask():
+    #selects the selected item and then deletes it 
+    selected=listbox_task.curselection()
+    listbox_task.delete(selected[0])
+#Executes this to mark completed 
 
-		if task:
+def markcompleted():
+    marked=listbox_task.curselection()
+    temp=marked[0]
+    #store the text of selected item in a string
+    temp_marked=listbox_task.get(marked)
+    #update it 
+    temp_marked=temp_marked+" ✔"
+    #delete it then insert it 
+    listbox_task.delete(temp)
+    listbox_task.insert(temp,temp_marked)
+#to hold items in a listbox
+listbox_task=Listbox(frame_task,bg="black",fg="white",height=15,width=50,font = "Helvetica")  
+listbox_task.pack(side=tkinter.LEFT)
 
-			self.tasks.append(task)
+#Scrolldown in case the total list exceeds the size of the given window 
+scrollbar_task=Scrollbar(frame_task)
+scrollbar_task.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+listbox_task.config(yscrollcommand=scrollbar_task.set)
+scrollbar_task.config(command=listbox_task.yview)
 
-			self.task_list.addItem(task)
 
-			self.input_field.clear()
+#Button widget 
+entry_button=Button(window,text="Add task",width=50,command=entertask)
+entry_button.pack(pady=3)
 
-	def delete_task(self):
+delete_button=Button(window,text="Delete selected task",width=50,command=deletetask)
+delete_button.pack(pady=3)
 
-		task=self.task_list.currentRow()
+mark_button=Button(window,text="Mark as completed ",width=50,command=markcompleted)
+mark_button.pack(pady=3)
 
-		self.task_list.takeItem(task)
 
-if __name__ == "__main__":
-
-	app = QApplication(sys.argv)
-
-	window = ToDoApp ()
-
-	window.show()
-
-	sys.exit(app.exec_())
+window.mainloop()
